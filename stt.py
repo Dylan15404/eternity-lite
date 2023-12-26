6875513b6f107b24433c0d8c90730cc1500ea7e1
Dylan <oldvic4bp@gmail.com>
2023-12-26 13:55:45 +0000

    v1.2.1
    -implemented phrase time limit
    -implemented sample rate control

--- stt.py
+++ stt.py
@@ -4,9 +4,12 @@ import time
 import whisper
 import integration
 
+SAMPLE_RATE = 32000
+PHRASE_TIME_LIMIT = 5
+
 p = pyaudio.PyAudio()
 r = sr.Recognizer()
-mic = sr.Microphone()
+mic = sr.Microphone(sample_rate=SAMPLE_RATE)
 
 def start_whisper(callword):
     def callback(r, audio): #function thats called whenever audio is detected
@@ -18,6 +21,6 @@ def start_whisper(callword):
         r.adjust_for_ambient_noise(source)
         print("Recognizing...")
 
-    stop_listening = r.listen_in_background(mic, callback) #listens in background and calls callback when there is audio
+    stop_listening = r.listen_in_background(mic, callback, phrase_time_limit=PHRASE_TIME_LIMIT) #listens in background and calls callback when there is audio
     for _ in range(50): time.sleep(0.1)
     while True: time.sleep(0.1)
